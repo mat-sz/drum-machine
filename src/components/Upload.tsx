2b97092b554b7f783dc96e5475b4fa8a0ba65af8
Mat Sz <contact@matsz.dev>
2019-12-26 16:24:07 +0100

    moved onDrop to a const to prevent rerenders

--- src/components/Upload.tsx
+++ src/components/Upload.tsx
@@ -5,31 +5,33 @@ import uuidv4 from 'uuid/v4';
 import { Track as TrackType } from '../Types';
 
 const Upload = ({ addTrack } : { addTrack: (track: TrackType) => void }) => {
+    const onDrop = (acceptedFiles: File[]) => {
+        if (acceptedFiles.length === 0)
+            return;
+
+        for (let file of acceptedFiles) {
+            if (!file)
+                continue;
+
+            const reader = new FileReader();
+
+            reader.addEventListener("load", () => {
+                const audio = new Audio(reader.result as string);
+                const track = {
+                    id: uuidv4(),
+                    name: file.name,
+                    audio: audio,
+                    beats: (new Array(16)).fill(0),
+                };
+                addTrack(track);
+            }, false);
+
+            reader.readAsDataURL(file);
+        }
+    };
+
     return (
-        <Dropzone onDrop={acceptedFiles => {
-            if (acceptedFiles.length === 0)
-                return;
-
-            for (let file of acceptedFiles) {
-                if (!file)
-                    continue;
-
-                const reader = new FileReader();
-
-                reader.addEventListener("load", () => {
-                    const audio = new Audio(reader.result as string);
-                    const track = {
-                        id: uuidv4(),
-                        name: file.name,
-                        audio: audio,
-                        beats: (new Array(16)).fill(0),
-                    };
-                    addTrack(track);
-                }, false);
-
-                reader.readAsDataURL(file);
-            }
-        }}>
+        <Dropzone onDrop={onDrop}>
             {({ getRootProps, getInputProps }) => (
             <section>
                 <div {...getRootProps()} className="dropzone">
